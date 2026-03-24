aa559bbe9832468923f6d7676a121240e0f18eb6
DragAndPlugin <axel.rossetto24@gmail.com>
2026-03-24 17:37:34 +0100

    Finished behaviors of array variable related commands

--- js/plugins/Drag_VisualEvent_ExpandedEventCommands.js
+++ js/plugins/Drag_VisualEvent_ExpandedEventCommands.js
@@ -32,9 +32,11 @@ Drag.VisualEvent_ExpandedEventCommands.version = "1.0.0";
 		itemList: {type: "text", name: "Items", default: "", isList: true},
 		arrayItem: {type: "text", name: "Item", default: ""},
 		fillArrayItem: {type: "text", name: "Fill Item", default: ""},
+		separator: {type: "text", name: "Separator", default: ""},
 		arrayGetMode: {type: "select", options: ["Specify Index", "Random"], name: "Get Mode", data: "data-dataType='number'", default: 0},
 		arrayItemVariable: {type: "select", options: ["Specify", "Variable"], name: "Item", data: "data-dataType='number'", default: 0},
 		removeArrayItem: {type: "checkbox", name: "Remove Item From Array", default: false, showName: true},
+		noDuplicateArrayItems: {type: "checkbox", name: "No Duplicate Items", default: false, showName: true},
 	});
 	
 	Object.assign(Drag.VisualEvent.interactiveInputs, {
@@ -121,4 +123,198 @@ Drag.VisualEvent_ExpandedEventCommands.version = "1.0.0";
 		
 		return weightedValues[roll];
 	};
+	
+	// --------------------------------------------------------------------------------------------------------------------------------
+	// Variable Array
+	
+	Game_Interpreter.prototype.command_create_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 1)
+			$gameVariables.setValue(params[0], params.slice(1));
+		
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_set_item_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 3) {
+			const array = $gameVariables.value(params[0]);
+			const index = params[1] >= 0 ? Math.min(params[1], array.length - 1) : Math.max(array.length + params[1], 0);
+			const value = params[2] === 0 ? params[3] : $gameVariables.value(params[3]);
+			if (Array.isArray(array))
+				array[index] = value;
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_add_item_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 4) {
+			const array = $gameVariables.value(params[0]);
+			const index = params[1] >= 0 ? Math.min(params[1], array.length - 1) : Math.max(array.length + params[1] + 1, 0);
+			const value = params[2] === 0 ? params[3] : $gameVariables.value(params[3]);
+			if (Array.isArray(array) && (!params[4] || !array.includes(value)))
+				array.splice(index, 0, value);
+				
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_get_item_variable_array = function(params) {
+		if (params[0] > 0 && params[2] > 0 && params.length > 3) {
+			const array = $gameVariables.value(params[0]);
+			const index = params[1] >= 0 ? Math.min(params[1], array.length - 1) : Math.max(array.length + params[1], 0);
+			if (Array.isArray(array)) {
+				const value = array[index];
+				$gameVariables.setValue(params[2], value);
+				if (params[3])
+					array.splice(index, 1);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_get_random_item_variable_array = function(params) {
+		if (params[0] > 0 && params[1] > 0 && params.length > 2) {
+			const array = $gameVariables.value(params[0]);
+			if (Array.isArray(array)) {
+				const index = array[Math.floor(Math.random() * array.length)];
+				const value = array[index];
+				$gameVariables.setValue(params[1], value);
+				if (params[2])
+					array.splice(index, 1);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_shuffle_items_variable_array = function(params) {
+		if (params[0] > 0) {
+			const array = $gameVariables.value(params[0]);
+			if (Array.isArray(array))
+				array.sort(() => Math.random() - 0.5);
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_sort_items_variable_array = function(params) {
+		if (params[0] > 0) {
+			const array = $gameVariables.value(params[0]);
+			if (Array.isArray(array))
+				array.sort();
+		}
+		return true;
+	};	
+	
+	Game_Interpreter.prototype.command_merge_variable_array = function(params) {
+		if (params[0] > 0 && params[1] > 0 && params.length > 2) {
+			const array1 = $gameVariables.value(params[0]);
+			const array2 = $gameVariables.value(params[1]);
+			if (Array.isArray(array1) && Array.isArray(array2)) {
+				if (!params[2])
+					$gameVariables.setValue(params[0], array1.concat(array2));
+				else 
+					for (const item in array2)
+						if (!array1.includes(item))
+							array2.push(item);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_reverse_items_variable_array = function(params) {
+		if (params[0] > 0) {
+			const array = $gameVariables.value(params[0]);
+			if (Array.isArray(array))
+				array.reverse();
+		}
+		return true;
+	};	
+	
+	Game_Interpreter.prototype.command_variable_array_includes_item = function(params) {
+		if (params[0] > 0 && params.length > 3 && params[3] > 0) {
+			const array = $gameVariables.value(params[0]);
+			const value = params[1] === 0 ? params[2] : $gameVariables.value(params[2]);
+			if (Array.isArray(array))
+				$gameSwitches.setValue(params[3], array.includes(value));
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_variable_array_includes_item_exec = function(params) {
+		if (params[0] > 0 && params.length > 2) {
+			const array = $gameVariables.value(params[0]);
+			const value = params[1] === 0 ? params[2] : $gameVariables.value(params[2]);
+			const result = array.includes(value);
+			
+			this._branch[this._indent] = result;
+			if (this._branch[this._indent] === false)
+				this.skipBranch();
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_variable_array_not_includes_item_exec = function(params) {
+		if (this._branch[this._indent] !== false) {
+			this.skipBranch();
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_replace_item_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 2) {
+			const array = $gameVariables.value(params[0]);
+			const value1 = params[1] === 0 ? params[2] : $gameVariables.value(params[2]);
+			const value2 = params[3] === 0 ? params[4] : $gameVariables.value(params[4]);
+			if (Array.isArray(array) && array.includes(value1))
+				array.splice(array.indexOf(value1), 0, value2);
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_set_variable_array_length = function(params) {
+		if (params[0] > 0 && params.length > 3) {
+			const array = $gameVariables.value(params[0]);
+			const length = params[1];
+			const value = params[2] === 0 ? params[3] : $gameVariables.value(params[3]);
+			if (Array.isArray(array)) {
+				const fillArray = length > array.length ? new Array(array.length - length).fill(value) : [];
+				array.length = length;
+				array.concat(fillArray);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_remove_item_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 2) {
+			const array = $gameVariables.value(params[0]);
+			const value = params[1] === 0 ? params[2] : $gameVariables.value(params[2]);
+			if (Array.isArray(array)) {
+				const index = array.indexOf(value);
+				if (index > -1)
+					array.splice(index, 1);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_remove_index_variable_array = function(params) {
+		if (params[0] > 0 && params.length > 1) {
+			const array = $gameVariables.value(params[0]);
+			if (Array.isArray(array)) {
+				const index = params[1] >= 0 ? Math.min(params[1], array.length - 1) : Math.max(array.length + params[1], 0);
+				array.splice(index, 1);
+			}
+		}
+		return true;
+	};
+	
+	Game_Interpreter.prototype.command_join_items_variable_array = function(params) {
+		if (params[0] > 0 && params[2] > 0 && params.length > 2) {
+			const array = $gameVariables.value(params[0]);
+			const separator = params[1];
+			if (Array.isArray(array))
+				$gameVariables.setValue(params[2], array.join(separator));
+		}
+		return true;
+	};
+	
 })();
